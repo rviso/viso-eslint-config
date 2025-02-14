faf47b2f12f030328271aa0b29c8a61f822abb4e
Lingann <Lingann.M@gmail.com>
2025-02-14 21:12:42 +0800

    ✨ feat: 更新 tsconfig-sort-keys.js，关闭 tsconfig 排序规则

--- packages/standard/libs/tsconfig-sort-keys.js
+++ packages/standard/libs/tsconfig-sort-keys.js
@@ -2,6 +2,6 @@ module.exports = {
   plugins: ['tsconfig-sort'],
   rules: {
     'jsonc/sort-keys': 'off', // 关闭其他排序规则
-    'tsconfig-sort/tsconfig-sort': 'error' // 启用规则
+    'tsconfig-sort/tsconfig-sort': 'off' // 启用规则
   }
 }
